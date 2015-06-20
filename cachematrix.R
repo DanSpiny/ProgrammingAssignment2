56130406163c4f8f682ba99aacf7d037286de174
Dan in Parallel <DanP@Dans-MacBook-Pro.local>
2015-06-20 15:55:18 +0300

    Adds implementation to makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Pair of functions presented in this module
+# perform caching the inverse of a matrix.
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(stored_mat = matrix()) {
+    # Creates a special "matrix" object that can cache its inverse
+    #
+    # Args: 
+    #   x - square invertible matrix
+    # Returns: 
+    #   "matrix" object that can cashe the inverse version of x
+    
+    inv <- NULL
+    
+    set <- function(input_mat) {
+        # given the matrix, sets the variables in this closure
+        stored_mat <<- input_mat
+        inv <<- NULL
+    }
+    
+    get <- function(){
+        # Gets the stored matrix
+        stored_mat    
+    }
+    
+    set_inv <- function(inv_mat){
+        # Sets the cached inverse matrix of the stored one
+        inv <<- inv_mat
+    }
+    
+    get_inv <- function() {
+        # Gets the cached inverse matrix of the stored one
+        inv
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
-
+    
+    # returns the list of getters and setters to work with special "matrix" obj
+    list(get = get, set = set, get_inv = get_inv, set_inv = set_inv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat_obj, ...) {
+    # Computes the inverse of the special "matrix"
+    # returned by makeCacheMatrix abovea
+    #
+    # If the inverse has already been calculated (and the matrix has not changed),
+    # then the cachesolve should retrieve the inverse from the cache.
+    #
+    # Args:
+    #   mat_obj - special "matrix" object returned by makeCacheMatrix
+    # Returns:
+    #   inverse matrix of the one stored in the special "matrix" object mat_obj
+    inv <- mat_obj$get_inv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- mat_obj$get()
+    inv <- solve(data, ...)
+    mat_obj$set_inv(inv)
+    inv
 }
